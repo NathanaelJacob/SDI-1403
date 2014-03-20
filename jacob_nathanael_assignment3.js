619639ba493fefa3b261a9a1fbdb6d7e95a40153
NathanaelJacob <dondiegoreserve@yahoo.com>
2014-03-20 16:21:44 -0400

    Set my global variables

--- jacob_nathanael_assignment3.js
+++ jacob_nathanael_assignment3.js
@@ -1,1 +1,7 @@
 alert("JavaScript works!");
+
+//set global variables initially
+var name = prompt("What is your name?", "Nate");
+var temp = "", dirtyClothesLeft = true, whites = false;
+var mLofSoap = 0, numClothes = 0, typeOfSoap = "", typeOfSoftner = "", typesOfLiquids = "";
+var myCoins = [], quarters = 0, nickels = 0, dimes = 0;
