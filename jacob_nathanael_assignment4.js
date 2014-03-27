0315bda9af509c10043936d2d33b0c8ad725e890
NathanaelJacob <dondiegoreserve@yahoo.com>
2014-03-26 23:11:15 -0400

    Added first function

--- jacob_nathanael_assignment4.js
+++ jacob_nathanael_assignment4.js
@@ -1,1 +1,19 @@
 alert("JavaScript works!");
+
+//Nathanael Jacob
+// SDI 1403
+// Assignment 4
+
+
+
+//Find smallest value in an array greater than a given number
+function smallestGreaterThan(array, number) {
+	array = array.sort(function(a,b) {
+		return a - b;
+	});
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] > number)
+			return array[i];
+	}
+	return null;
+}
